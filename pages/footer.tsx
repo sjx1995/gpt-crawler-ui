3ccaee053ed81ed4626664dc1a578790dc8d4f35
sunly <sunjingxiang1995@gmail.com>
2023-11-24 01:00:31 +0800

    update: footer

--- pages/footer.tsx
+++ pages/footer.tsx
@@ -8,17 +8,18 @@ import styles from "@/styles/Home.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      © Sunly 2023 ·{" "}
-      <a href="https://github.com/sjx1995/gpt-crawler-ui" target="_blank">
-        GITHUB
-      </a>{" "}
-      ·{" "}
-      <a
-        href="https://chat.openai.com/g/g-neAR0jAgY-crawlergpt"
-        target="_blank"
-      >
-        GPT机器人
-      </a>
+      {`© Sunly 2023 · ${(
+        <a href="https://github.com/sjx1995/gpt-crawler-ui" target="_blank">
+          GITHUB
+        </a>
+      )} · ${(
+        <a
+          href="https://chat.openai.com/g/g-neAR0jAgY-crawlergpt"
+          target="_blank"
+        >
+          GPT机器人
+        </a>
+      )}`}
     </footer>
   );
 }
